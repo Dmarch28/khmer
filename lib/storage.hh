1eeddfa90b283807f4cbc792057efb1ff26c39b0
Tim Head <betatim@gmail.com>
2016-12-06 15:36:46 +0100

    Mofiy tests to understand new command-line argument

--- lib/storage.hh
+++ lib/storage.hh
@@ -239,6 +239,7 @@ protected:
 class NibbleStorage : public Storage
 {
 protected:
+    // table size is measured in number of entries in the table, not in bytes
     std::vector<uint64_t> _tablesizes;
     size_t _n_tables;
     uint64_t _occupied_bins;

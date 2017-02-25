9d9825be57cda8164e3ec5840f28af18f3f58a74
C. Titus Brown <titus@idyll.org>
2017-02-24 21:00:47 -0800

    split tests out to test_sequence_validation

--- tests/test_sequence_validation.py
+++ tests/test_sequence_validation.py
@@ -37,48 +37,17 @@
 # Tests for the ReadParser and Read classes.
 from __future__ import print_function
 from __future__ import absolute_import
-from khmer import _Countgraph, _Counttable, _SmallCountgraph, _SmallCounttable
-from khmer import _Nodegraph, _Nodetable
-from khmer import ReadParser, Nodegraph, _GraphLabels
+from khmer import ReadParser, Counttable, Nodegraph
+from screed import Record
 from . import khmer_tst_utils as utils
 import pytest
+from functools import reduce  # pylint: disable=redefined-builtin
 
 
-PRIMES_1m = [1000003, 1009837]
-
-
-# all the table types!
-@pytest.fixture(params=[_Countgraph, _Counttable, _SmallCountgraph,
-                        _SmallCounttable, _Nodegraph, _Nodetable])
-def Tabletype(request):
-    return request.param
-
-
-# all the counting types!
-@pytest.fixture(params=[_Countgraph, _Counttable, _SmallCountgraph,
-                        _SmallCounttable])
-def Countingtype(request):
-    return request.param
-
-
-# all the graph types!
-@pytest.fixture(params=[_Countgraph, _Nodegraph])
-def Graphtype(request):
-    return request.param
-
-
-@pytest.yield_fixture
-def reads():
-    infile = utils.get_test_data('valid-read-testing.fq')
-    reads = ReadParser(infile)
-    yield reads
-    reads.close()
-
-
-def test_read_cleaning_consume_seqfile(Countingtype):
+def test_read_cleaning_consume_seqfile():
     infile = utils.get_test_data('valid-read-testing.fq')
 
-    x = Countingtype(15, PRIMES_1m)
+    x = Counttable(15, int(1e6), 4)
     x.consume_seqfile(infile)
 
     # the relevant read will automatically get uppercased => abundance of 2
@@ -87,16 +56,18 @@ def test_read_cleaning_consume_seqfile(Countingtype):
 
     # the 2nd read with this k-mer in it has an N in it.
     kmer = "CCTCATCGGCACCAG"
-    assert x.get(kmer) == 1               # this should be 2 in the future
+    assert x.get(kmer) == 2
 
     # the 2nd read with this k-mer in it has a Z in it
     kmer = "ACTGAGCTTCATGTC"
-    assert x.get(kmer) == 1               # this should be 2 in the future
+    assert x.get(kmer) == 2
 
 
-def test_read_cleaning_consume_read_by_read(Countingtype, reads):
-    x = Countingtype(15, PRIMES_1m)
-    for read in reads:
+def test_read_cleaning_consume_read_by_read():
+    infile = utils.get_test_data('valid-read-testing.fq')
+
+    x = Counttable(15, int(1e6), 4)
+    for read in ReadParser(infile):
         x.consume(read.sequence)          # consume raw sequence
 
     # the relevant read will be entirely ignored
@@ -115,9 +86,11 @@ def test_read_cleaning_consume_read_by_read(Countingtype, reads):
     assert x.get(kmer) == 2
 
 
-def test_read_cleaning_consume_read_by_read_cleaned_seq(Countingtype, reads):
-    x = Countingtype(15, PRIMES_1m)
-    for read in reads:
+def test_read_cleaning_consume_read_by_read_cleaned_seq():
+    infile = utils.get_test_data('valid-read-testing.fq')
+
+    x = Counttable(15, int(1e6), 4)
+    for read in ReadParser(infile):
         x.consume(read.cleaned_seq)       # consume cleaned_seq
 
     # the relevant read will be cleaned & loaded
@@ -133,78 +106,86 @@ def test_read_cleaning_consume_read_by_read_cleaned_seq(Countingtype, reads):
     assert x.get(kmer) == 2
 
 
-def test_read_cleaning_abundance_distribution(Countingtype):
+def test_read_cleaning_abundance_distribution():
     infile = utils.get_test_data('valid-read-testing.fq')
 
-    x = Countingtype(15, PRIMES_1m)
-    y = _Nodegraph(15, PRIMES_1m)
+    x = Counttable(15, int(1e6), 4)
+    y = Nodegraph(15, int(1e6), 4)
 
     x.consume_seqfile(infile)
 
     dist = x.abundance_distribution(infile, y)
-    assert dist[1] == 41
-    assert dist[2] == 42
+    assert dist[1] == 29                  # k-mers with non-ACGTN => ignored.
+    assert dist[2] == 68
 
 
-def test_read_cleaning_trim_functions_lowercase(Tabletype, reads):
+def test_read_cleaning_trim_functions_lowercase():
+    infile = utils.get_test_data('valid-read-testing.fq')
+
     # read this in using "approved good" behavior w/cleaned_seq
-    x = Tabletype(8, PRIMES_1m)
-    for read in reads:
+    x = Counttable(8, int(1e6), 4)
+    for read in ReadParser(infile):
         x.consume(read.cleaned_seq)       # consume cleaned_seq
+
+    # all of these functions will fail to do anything, b/c lowercase != valid
+    # BUT they will not raise an exception, either.
 
     s = "caggcgcccaccaccgtgccctccaacctgatggt"
     _, where = x.trim_on_abundance(s, 1)
-    assert where == 35                    # in future, should be '0'
+    assert where == 0
 
-    _, where = x.trim_below_abundance(s, 2)
-    assert where == 35                    # in future, should be ?? @CTB
+    _, where = x.trim_below_abundance(s, 0)
+    print(x.get_kmer_counts(s))
+    assert where == 35                    # stays at 35 (abunds all == 0)
 
     posns = x.find_spectral_error_positions(s, 1)
-    assert posns == []                    # in future, should do same
+    assert posns == []
 
 
-def test_read_cleaning_trim_functions_N(Tabletype, reads):
+def test_read_cleaning_trim_functions_N():
+    infile = utils.get_test_data('valid-read-testing.fq')
+
     # read this in using "approved good" behavior w/cleaned_seq
-    x = Tabletype(8, PRIMES_1m)
-    for read in reads:
+    x = Counttable(8, int(1e6), 4)
+    for read in ReadParser(infile):
         x.consume(read.cleaned_seq)       # consume cleaned_seq
 
     s = "ACTGGGCGTAGNCGGTGTCCTCATCGGCACCAGC"
     _, where = x.trim_on_abundance(s, 1)
-    assert where == 0                     # in future, should be ??
+    assert where == 11
 
     _, where = x.trim_below_abundance(s, 2)
-    assert where == 0                     # in future, should be ??
+    assert where == 34
 
-    with pytest.raises(ValueError):
-        posns = x.find_spectral_error_positions(s, 1)
-    # assert posns == []                    # in future, should return []
+    posns = x.find_spectral_error_positions(s, 1)
+    assert posns == [11]
 
 
-def test_read_cleaning_trim_functions_bad_dna(Tabletype, reads):
+def test_read_cleaning_trim_functions_bad_dna():
+    infile = utils.get_test_data('valid-read-testing.fq')
+
     # read this in using "approved good" behavior w/cleaned_seq
-    x = Tabletype(8, PRIMES_1m)
-    for read in reads:
+    x = Counttable(8, int(1e6), 4)
+    for read in ReadParser(infile):
         x.consume(read.cleaned_seq)       # consume cleaned_seq
 
     s = "CCGGCGTGGTTZZYAGGTCACTGAGCTTCATGTC"
     _, where = x.trim_on_abundance(s, 1)
-    assert where == 0                     # in future, should be ??
+    assert where == 11
 
     _, where = x.trim_below_abundance(s, 2)
-    assert where == 0                     # in future, should be ??
+    assert where == 34
 
-    with pytest.raises(ValueError):
-        posns = x.find_spectral_error_positions(s, 1)
-    # assert posns == []                    # in future, should return [11]
+    posns = x.find_spectral_error_positions(s, 1)
+    assert posns == [11]
 
 
-def test_read_cleaning_output_partitions(Graphtype):
+def test_read_cleaning_output_partitions():
     infile = utils.get_test_data('valid-read-testing.fq')
     savepath = utils.get_temp_filename('foo')
 
     # read this in using "approved good" behavior w/cleaned_seq
-    x = Graphtype(8, PRIMES_1m)
+    x = Nodegraph(8, int(1e6), 4)
     for read in ReadParser(infile):
         x.consume(read.cleaned_seq)       # consume cleaned_seq
 
@@ -224,24 +205,23 @@ def test_read_cleaning_output_partitions(Graphtype):
 
     read_names = [read.name for read in ReadParser(savepath)]
     print(read_names)
-    assert len(read_names) == 4
+    assert len(read_names) == 6
 
-    print(read_names)
-    assert '895:1:1:1246:14654 1:N:0:NNNNN\t1\t1' in read_names
-    assert '895:1:1:1248:9583 1:N:0:NNNNN\t2\t2' in read_names
-    assert '895:1:1:1252:19493 1:N:0:NNNNN\t3\t3' in read_names
+    assert '895:1:1:1246:14654 1:N:0:NNNNN\t1' in read_names
+    assert '895:1:1:1248:9583 1:N:0:NNNNN\t2' in read_names
+    assert '895:1:1:1252:19493 1:N:0:NNNNN\t3' in read_names
 
-    assert 'lowercase_to_uppercase\t5\t1' in read_names
-
-    assert 'n_in_read\t6\t2' not in read_names
-    assert 'zy_in_read\t7\t3' not in read_names
+    assert 'lowercase_to_uppercase\t1' in read_names
+    assert 'n_in_read\t2' in read_names
+    assert 'zy_in_read\t3' in read_names
 
 
-def test_read_cleaning_trim_on_stoptags(Graphtype):
+def test_read_cleaning_trim_on_stoptags():
     infile = utils.get_test_data('valid-read-testing.fq')
+    savepath = utils.get_temp_filename('foo')
 
     # read this in using "approved good" behavior w/cleaned_seq
-    x = Graphtype(8, PRIMES_1m)
+    x = Nodegraph(8, int(1e6), 4)
     for read in ReadParser(infile):
         x.consume(read.cleaned_seq)       # consume cleaned_seq
 
@@ -256,79 +236,13 @@ def test_read_cleaning_trim_on_stoptags(Graphtype):
     x.add_stop_tag(kmer)
 
     _, pos = x.trim_on_stoptags('caggcgcccaccaccgtgccctccaacctgatggt')
-    assert pos == 6                       # should be ?? in future
+    assert pos == 35                      # no stoptag b/c lowercase => no trim
+
+    _, pos = x.trim_on_stoptags('ACTGGGCGTAGNCGGTGTCCTCATCGGCACCAGC')
+    assert pos == 6                       # N ignored
 
     _, pos = x.trim_on_stoptags('CCGGCGTGGTTZZYAGGTCACTGAGCTTCATGTC')
-    assert pos == 0                       # should be 6 in future
-
-    _, pos = x.trim_on_stoptags('CCGGCGTGGTTZZYAGGTCACTGAGCTTCATGTC')
-    assert pos == 0                       # should be 6 in future
-
-
-def test_consume_seqfile_and_tag(Graphtype):
-    infile = utils.get_test_data('valid-read-testing.fq')
-
-    # read this in consume_and_tag
-    x = Graphtype(8, PRIMES_1m)
-    x.consume_seqfile_and_tag(infile)
-    _, n_tags = x.count_partitions()
-    assert n_tags == 4                    # total # of tags
-
-
-def test_consume_partitioned_seqfile(Graphtype):
-    infile = utils.get_test_data('valid-read-testing.fq')
-
-    # read this in consume_and_tag
-    x = Graphtype(15, PRIMES_1m)
-    x.consume_partitioned_fasta(infile)
-    n_partitions, n_tags = x.count_partitions()
-    assert n_partitions == 4
-    assert n_tags == 0
-
-
-def test_output_partitioned_file(Graphtype):
-    infile = utils.get_test_data('valid-read-testing.fq')
-    savepath = utils.get_temp_filename('foo')
-
-    # read this in consume_and_tag
-    x = Graphtype(15, PRIMES_1m)
-    x.consume_partitioned_fasta(infile)
-    x.output_partitions(infile, savepath)
-
-    read_names = [read.name for read in ReadParser(savepath)]
-    read_names = set(read_names)
-
-    good_names = ['895:1:1:1246:14654 1:N:0:NNNNN\t1\t5',
-                  '895:1:1:1248:9583 1:N:0:NNNNN\t2\t2',
-                  '895:1:1:1252:19493 1:N:0:NNNNN\t3\t3',
-                  '895:1:1:1255:18861 1:N:0:NNNNN\t4\t8',
-                  'lowercase_to_uppercase\t5\t5',
-                  '895:1:1:1255:18861 1:N:0:NNNNN\t8\t8']
-    good_names = set(good_names)
-
-    assert good_names == read_names
-
-
-def test_consume_seqfile_and_tag_with_labels(Graphtype):
-    infile = utils.get_test_data('valid-read-testing.fq')
-
-    # read this in consume_and_tag
-    graph = Graphtype(15, PRIMES_1m)
-    x = _GraphLabels(graph)
-    x.consume_seqfile_and_tag_with_labels(infile)
-
-    assert x.n_labels() == 6
-
-
-def test_consume_partitioned_seqfile_and_label(Graphtype):
-    infile = utils.get_test_data('valid-read-testing.fq')
-
-    # read this in consume_and_tag
-    graph = Graphtype(15, PRIMES_1m)
-    x = _GraphLabels(graph)
-    x.consume_partitioned_fasta_and_tag_with_labels(infile)
-
-    assert x.n_labels() == 6
+    assert pos == 6                       # ZZY ignored
 
 
 # vim: set filetype=python tabstop=4 softtabstop=4 shiftwidth=4 expandtab:

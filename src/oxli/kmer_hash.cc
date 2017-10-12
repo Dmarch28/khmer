224822bebbd92924c1e1ca7f6b53ff7ec04db0af
Tim Head <betatim@gmail.com>
2017-10-12 16:50:42 +0200

    Update include directories for liboxli

--- src/oxli/kmer_hash.cc
+++ src/oxli/kmer_hash.cc
@@ -228,6 +228,8 @@ HashIntoType _hash_cyclic(const std::string& kmer, const WordLength k,
 {
     const std::string rev = oxli::_revcomp(kmer);
     static CyclicHash<uint64_t> fwd_hasher(k);
+    static CyclicHash<uint64_t> rev_hasher(k);
+
     for (WordLength i = 0; i < k; ++i) {
         fwd_hasher.eat(kmer[i]);
     }
@@ -239,7 +241,6 @@ HashIntoType _hash_cyclic(const std::string& kmer, const WordLength k,
         return h;
     }
 
-    static CyclicHash<uint64_t> rev_hasher(k);
     for (WordLength i = 0; i < k; ++i) {
         rev_hasher.eat(rev[i]);
     }
